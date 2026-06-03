2252a1c62236a5ee9f1ca181f6b4faec91cab410
Arnold Vink <info@arnoldvink.com>
2026-06-03 11:48:21 +0200

    - Updated assembly version.

--- FpsOverlayer/Properties/AssemblyVersion.cs
+++ FpsOverlayer/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 //Version information for assembly
-[assembly: AssemblyVersion("2.7.0.0")]
-[assembly: AssemblyFileVersion("2.7.0.0")]
+[assembly: AssemblyVersion("2.8.0.0")]
+[assembly: AssemblyFileVersion("2.8.0.0")]
